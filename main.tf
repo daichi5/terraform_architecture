c5e9c41d9b0fd7672a02067d4a7a11b655a2a41d
sawa <sw41dc@gmail.com>
2020-06-01 23:08:33 +0900

    Add security group module

--- main.tf
+++ main.tf
@@ -137,27 +137,10 @@ resource "aws_route" "private_1" {
     destination_cidr_block = "0.0.0.0/0"
 }
 
-# securities
-
-resource "aws_security_group" "example" {
-    name = "example"
+module "example_sg" {
+    source = "./security_group"
+    name = "module-sg"
     vpc_id = aws_vpc.example.id
-}
-
-resource "aws_security_group_rule" "ingress_example" {
-    type = "ingress"
-    from_port = "80"
-    to_port = "80"
-    protocol = "tcp"
+    port = 80
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws_security_group.example.id
-}
-
-resource "aws_security_group_rule" "egress_example" {
-    type = "egress"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = aws_security_group.example.id
 }
