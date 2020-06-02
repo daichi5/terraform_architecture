1ecf2e2d7bed66436c3cb00a5547fa5f88cd3930
sawa <sw41dc@gmail.com>
2020-06-03 00:22:38 +0900

    Add ALB Listener

--- main.tf
+++ main.tf
@@ -229,6 +229,21 @@ output "alb_dns_name" {
     value = aws_lb.example.dns_name
 }
 
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = "80"
+    protocol = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "this is http."
+            status_code = "200"
+        }
+    }
+}
+
 # security groups
 
 module "http_sg" {
