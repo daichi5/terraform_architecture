064621608964304c954bb3b06eabf03efb283d0d
sawa <sw41dc@gmail.com>
2020-06-03 23:36:37 +0900

    Add ACM

--- main.tf
+++ main.tf
@@ -291,3 +291,13 @@ resource "aws_route53_record" "example" {
 output "domain_name" {
     value = aws_route53_record.example.name
 }
+
+resource "aws_acm_certificate" "example" {
+    domain_name = aws_route53_record.example.name
+    subject_alternative_names = []
+    validation_method = "DNS"
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
