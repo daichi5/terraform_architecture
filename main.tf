aeaef047a0ef4e66d4ffcfa293f4570d2f15de13
sawa <sw41dc@gmail.com>
2020-06-08 01:41:31 +0900

    Add CloudWatch Logs

--- main.tf
+++ main.tf
@@ -404,6 +404,7 @@ resource "aws_ecs_task_definition" "example" {
     network_mode = "awsvpc"
     requires_compatibilities = ["FARGATE"]
     container_definitions = file("./container_definitions.json")
+    execution_role_arn = module.ecs_task_execution_role.iam_role_arn
 }
 
 resource "aws_ecs_service" "example" {
@@ -437,3 +438,30 @@ resource "aws_ecs_service" "example" {
     depends_on = [aws_lb_target_group.example]
 }
 
+# Cloud Watch Logs
+
+resource "aws_cloudwatch_log_group" "for_ecs" {
+    name = "/ecs/example"
+    retention_in_days = 180
+}
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+    arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+    source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+    statement {
+        effect = "Allow"
+        actions = ["ssm:GetParameters", "kms:Decrypt"]
+        resources = ["*"]
+    }
+}
+
+module "ecs_task_execution_role" {
+    source = "./iam_role"
+    name = "ecs-task-execution"
+    identifier = "ecs-tasks.amazonaws.com"
+    policy = data.aws_iam_policy_document.ecs_task_execution.json
+}
