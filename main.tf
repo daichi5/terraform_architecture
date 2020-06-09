d0a32944ff0bdc40c8307e898988ae696117eb51
sawa <sw41dc@gmail.com>
2020-06-09 22:57:44 +0900

    Add KMS

--- main.tf
+++ main.tf
@@ -518,3 +518,17 @@ resource "aws_cloudwatch_event_target" "example_batch" {
         }
     }
 }
+
+# KMS
+
+resource "aws_kms_key" "example" {
+    description = "Example Customer Master Key"
+    enable_key_rotation = true
+    is_enabled = true
+    deletion_window_in_days = 30
+}
+
+resource "aws_km_alias" "example" {
+    name = "alias/example"
+    target_key_id = aws_kms_key.example.key_id
+}
