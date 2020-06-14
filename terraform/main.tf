392e60d2c6dbd6e78c0d837815b7d24c314abc0c
sawa <sw41dc@gmail.com>
2020-06-15 00:39:52 +0900

    Add CodePipeline with GitHub WebHook

--- terraform/main.tf
+++ terraform/main.tf
@@ -798,3 +798,103 @@ resource "aws_s3_bucket" "artifact" {
     }
   }
 }
+
+resource "aws_codepipeline" "example" {
+  name = "example"
+  role_arn = module.codepipeline_role.iam_role_arn
+
+  stage {
+    name = "Source"
+
+    action {
+      name = "Sourec"
+      category = "Source"
+      owner = "ThirdParty"
+      provider = "GitHub"
+      version = 1
+      output_artifacts = ["Source"]
+
+      configuration = {
+        Owner = "daichi5"
+        Repo = "terraform_architecture"
+        Branch = "master"
+        PollForSourceChanges = false
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name = "Build"
+      category = "Build"
+      owner = "AWS"
+      provider = "CodeBuild"
+      version = 1
+      input_artifacts = ["Source"]
+      output_artifacts = ["Build"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.example.id
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name = "Deploy"
+      category = "Deploy"
+      owner = "AWS"
+      provider = "ECS"
+      version = 1
+      input_artifacts = ["Build"]
+
+      configuration = {
+        ClusterName = aws_ecs_cluster.example.name
+        ServiceName = aws_ecs_service.example.name
+        FileName = "imagedefinitions.json"
+      }
+    }
+  }
+
+  artifact_store {
+    location = aws_s3_bucket.artifact.id
+    type = "S3"
+  }
+}
+
+resource "aws_codepipeline_webhook" "example" {
+  name = "example"
+  target_pipeline = aws_codepipeline.example.name
+  target_action = "Source"
+  authentication = "GITHUB_HMAC"
+
+  authentication_configuration {
+    secret_token = "RandomStringTokenMoreThan20Bytes!"
+  }
+
+  filter {
+    json_path = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+}
+
+provider "github" {
+  individual = true
+}
+
+resource "github_repository_webhook" "example" {
+  repository = "terraform_architecture"
+
+  configuration {
+    url = aws_codepipeline_webhook.example.url
+    secret = "RandomStringTokenMoreThan20Bytes!"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  events = ["push"]
+}
