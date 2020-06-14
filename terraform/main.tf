b13ea0c8d421d0109ac4f37ec53913db0a956509
sawa <sw41dc@gmail.com>
2020-06-14 13:59:20 +0900

    Add CodePipeline resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -754,3 +754,47 @@ resource "aws_codebuild_project" "example" {
     privileged_mode = true
   }
 }
+
+# CodePipeline
+
+data "aws_iam_policy_document" "codepipeline" {
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DescribeTasks",
+      "ecs:ListTasks",
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+      "iam:PassRole",
+    ]
+  }
+}
+
+module "codepipeline_role" {
+  source = "./iam_role"
+  name = "codepipeline"
+  identifier = "codepipeline.amazonaws.com"
+  policy = data.aws_iam_policy_document.codepipeline.json
+}
+
+resource "aws_s3_bucket" "artifact" {
+  bucket = "artifact-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
