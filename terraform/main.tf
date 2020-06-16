d74d5bb9f087596d84a67a6dc9eac65fdcdda7ad
sawa <sw41dc@gmail.com>
2020-06-16 23:35:54 +0900

    Add IAM role of EC2 for SSM

--- terraform/main.tf
+++ terraform/main.tf
@@ -898,3 +898,45 @@ resource "github_repository_webhook" "example" {
 
   events = ["push"]
 }
+
+# SSM
+
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "logs:PutLogEvents",
+      "logs:CreateLogStream",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+module "ec2_for_ssm_role" {
+  source = "./iam_role"
+  name = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.ec2_for_ssm.json
+}
+
+resource "aws_iam_instance_profile" "ec2_for_ssm" {
+  name = "ec2-for-ssm"
+  role = module.ec2_for_ssm_role.iam_role_name
+}
+
