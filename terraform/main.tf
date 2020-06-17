780cee40e97e929e7b641800911cdbf9c29fc13a
sawa <sw41dc@gmail.com>
2020-06-17 23:21:26 +0900

    Add SSM operation resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -951,3 +951,38 @@ resource "aws_instance" "example_for_operation" {
 output "operation_instance_id" {
   value = aws_instance.example_for_operation.id
 }
+
+resource "aws_s3_bucket" "operation" {
+  bucket = "operation-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "operation" {
+  name = "/operation"
+  retention_in_days = 180
+}
+
+resource "aws_ssm_document" "session_manager_run_shell" {
+  name = "SSM-SessionManagerRunShell"
+  document_type = "Session"
+  document_format = "JSON"
+
+  content = <<EOF
+  {
+    "schemaVersion": "1.0",
+    "description": "Document to hold regional settings for Session Manager",
+    "sessionType": "Standard_Stream",
+    "inputs": {
+      "s3BucketName": "${aws_s3_bucket.operation.id}",
+      "cloudWatchLogGroupName": "${aws_cloudwatch_log_group.operation.name}"
+    }
+  }
+EOF
+}
