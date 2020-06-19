e0db7a6eb9ca7364e05a77904e757dd061147733
sawa <sw41dc@gmail.com>
2020-06-19 22:17:31 +0900

    Add Firehose Streaming

--- terraform/main.tf
+++ terraform/main.tf
@@ -986,3 +986,55 @@ resource "aws_ssm_document" "session_manager_run_shell" {
   }
 EOF
 }
+
+# Logging
+
+resource "aws_s3_bucket" "cloudwatch_logs" {
+  bucket = "cloudwatch-logs-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "kinesis_data_firehose" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}",
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*",
+    ]
+  }
+}
+
+module "kinesis_data_firehose_role" {
+  source = "./iam_role"
+  name = "kinesis-data-firehose"
+  identifier = "firehose.amazonaws.com"
+  policy = data.aws_iam_policy_document.kinesis_data_firehose.json
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "example" {
+  name = "example"
+  destination = "s3"
+
+  s3_configuration {
+    role_arn = module.kinesis_data_firehose_role.iam_role_arn
+    bucket_arn = aws_s3_bucket.cloudwatch_logs.arn
+    prefix = "ecs-scheduled-tasks/example/"
+  }
+}
