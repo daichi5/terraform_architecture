0ae123b514c41947d029da71fc702dd5d021ca01
sawa <sw41dc@gmail.com>
2020-06-24 22:32:51 +0900

    Add GitHub Webhook for CodeBuild

--- terraform/main.tf
+++ terraform/main.tf
@@ -1151,3 +1151,41 @@ resource "aws_codebuild_project" "continuous_apply" {
 data "aws_ssm_parameter" "github_token" {
   name = "/continuous_apply/github_token"
 }
+
+resource "aws_codebuild_webhook" "continuous_apply" {
+  project_name = aws_codebuild_project.continuous_apply.name
+
+  filter_group {
+    filter {
+      type = "EVENT"
+      pattern = "PULL_REQUEST_CREATED"
+    }
+  }
+
+  filter_group {
+    filter {
+      type = "EVENT"
+      pattern = "PULL_REQUEST_UPDATED"
+    }
+  }
+
+  filter_group {
+    filter {
+      type = "EVENT"
+      pattern = "PULL_REQUEST_REOPENED"
+    }
+  }
+
+  filter_group {
+    filter {
+      type = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type = "HEAD_REF"
+      pattern = "master"
+    }
+  }
+
+}
