f46f05630a69b57e1e24fe0babfafcd0169b6f07
sawa <sw41dc@gmail.com>
2020-06-24 22:19:06 +0900

    Add CodeBuild project

--- terraform/main.tf
+++ terraform/main.tf
@@ -1114,3 +1114,40 @@ data "aws_iam_policy" "administrator_access" {
   arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
+resource "aws_codebuild_project" "continuous_apply" {
+  name = "continuous-apply"
+  service_role = module.continuous_apply_codebuild_role.iam_role_arn
+
+  source {
+    type = "GITHUB"
+    location = "https://github.com/daichi5/terraform_architecture.git"
+  }
+
+  artifacts {
+    type =  "NO_ARTIFACTS"
+  }
+
+  environment {
+    type = "LINUX_CONTAINER"
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "hashicorp/terraform:0.12.5"
+    privileged_mode = false
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws codebuild import-source-credentials \
+        --server-type GITHUB \
+        --auth-type PERSONAL_ACCESS_TOKEN \
+        --token $GITHUB_TOKEN
+    EOT
+
+    environment = {
+      GITHUB_TOKEN = data.aws_ssm_parameter.github_token.value
+    }
+  }
+}
+
+data "aws_ssm_parameter" "github_token" {
+  name = "/continuous_apply/github_token"
+}
